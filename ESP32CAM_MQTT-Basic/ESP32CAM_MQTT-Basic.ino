0f9c8f633b8eb5d1993d53a0e90d9a9a2c85beff
Hugo Escalpelo <hugoescalpelo@gmail.com>
2022-02-28 22:20:04 -0600

    Update ESP32CAM_MQTT-Basic.ino

--- ESP32CAM_MQTT-Basic/ESP32CAM_MQTT-Basic.ino
+++ ESP32CAM_MQTT-Basic/ESP32CAM_MQTT-Basic.ino
@@ -18,12 +18,12 @@
 #include <PubSubClient.h> //Biblioteca para conexion MQTT
 
 //Datos de WiFi
-const char* ssid = "********";  // Aquí debes poner el nombre de tu red
-const char* password = "********";  // Aquí debes poner la contraseña de tu red
+const char* ssid = "AXTEL XTREMO-18D6";  // Aquí debes poner el nombre de tu red
+const char* password = "038C18D6";  // Aquí debes poner la contraseña de tu red
 
 //Datos del broker MQTT
-const char* mqtt_server = "127.0.0.1"; // Si estas en una red local, coloca la IP asignada, en caso contrario, coloca la IP publica
-IPAddress server(127,0,0,1);
+const char* mqtt_server = "192.168.15.33"; // Si estas en una red local, coloca la IP asignada, en caso contrario, coloca la IP publica
+IPAddress server(192,168,15,33);
 
 // Objetos
 WiFiClient espClient; // Este objeto maneja los datos de conexion WiFi
@@ -98,7 +98,7 @@ void loop() {
     dtostrf(data, 1, 2, dataString);  // Esta es una función nativa de leguaje AVR que convierte un arreglo de caracteres en una variable String
     Serial.print("Contador: "); // Se imprime en monitor solo para poder visualizar que el evento sucede
     Serial.println(dataString);
-    client.publish("esp32/data", dataString); // Esta es la función que envía los datos por MQTT, especifica el tema y el valor
+    client.publish("codigoIoT/ejemplos/MQTT", dataString); // Esta es la función que envía los datos por MQTT, especifica el tema y el valor
   }// fin del if (timeNow - timeLast > wait)
 }// fin del void loop ()
 
